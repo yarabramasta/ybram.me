4a90a6c7c287a2ec4c246c1248615d48b041c9be
yarabramasta <yarabram111@gmail.com>
2023-01-24 22:41:10 +0700

    chore: setup lint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    domains: ['bucket.ybram.my.id']
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
