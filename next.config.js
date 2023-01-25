a5cc45ea372356ce9ce974a42a4ca3d1c6289c1a
yarabramasta <yarabram111@gmail.com>
2023-01-25 19:06:12 +0700

    chore: update static assets

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['bucket.ybram.my.id']
+  },
+  async redirects() {
+    return [
+      {
+        source: '/socials/twitter',
+        destination: 'https://twitter.com/yarabram',
+        permanent: true
+      },
+      {
+        source: '/socials/github',
+        destination: 'https://github.com/yarabramasta',
+        permanent: true
+      }
+    ];
   }
 };
 
