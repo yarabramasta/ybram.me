b4ca9f451100518ff89b5f01dbea5915a3804768
yarabramasta <yarabram111@gmail.com>
2023-01-28 18:41:34 +0700

    style: browser toolbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,8 +34,8 @@ const switzer = localFont({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  // if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
-  if (typeof window !== 'undefined') {
+  if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
+    // if (typeof window !== 'undefined') {
     getAnalytics(app);
     getPerformance(app);
   }
