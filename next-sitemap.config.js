d56828d00a41c19f0513a709052fb1dec01bee15
yarabramasta <yarabram111@gmail.com>
2023-02-01 14:34:49 +0700

    fix: update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,24 @@
+const SITE_URL = process.env.SITE_URL || 'https://ybram.me';
+const NEXT_SSG_FILES = [
+  '/*.json$',
+  '/*_buildManifest.js$',
+  '/*_middlewareManifest.js$',
+  '/*_ssgManifest.js$',
+  '/*.js$'
+];
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.ybram.me',
+  siteUrl: SITE_URL,
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: NEXT_SSG_FILES
+      }
+    ]
+  },
   priority: 0.9,
-  exclude: ['/api']
+  exclude: ['/api', '/auth*']
 };
--- src/pages/blog/[slug]/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from 'next';
-
-const BlogArticle: NextPage = () => {
-  return <></>;
-};
-
-export default BlogArticle;
