d047480a60d0a08bfd1008fc217cad8eb13d89c4
yarabramasta <yarabram111@gmail.com>
2023-02-03 15:07:47 +0700

    fix: firebase analytics cors

--- next.config.mjs
+++ next.config.mjs
@@ -23,7 +23,7 @@ const nextConfig = withMDX({
   async headers() {
     return [
       {
-        source: '/api/:path*',
+        source: '/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
