b9e0a5558da5ee767e8258870c2c893dd0ce11a8
yarabramasta <yarabram111@gmail.com>
2023-02-03 14:24:54 +0700

    fix(http-headers): update next config

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,26 @@ const nextConfig = withMDX({
   images: {
     domains: ['bucket.ybram.my.id', 'media.graphassets.com']
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+          }
+        ]
+      }
+    ];
+  },
   async redirects() {
     return [
       {
