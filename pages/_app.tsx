9a956bf3eaef843d20b70d04b9d5459fdb667e47
yarabramasta <yarabram111@gmail.com>
2023-02-05 13:47:56 +0700

    feat: vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,20 @@
 import '@/styles/globals.css';
 
-import type { AppType } from 'next/app';
-
 import Header from '@/ui/Header';
+import { Analytics } from '@vercel/analytics/react';
+import type { AppType } from 'next/app';
 
 const App: AppType = ({ Component, pageProps }) => {
   return (
-    <div className="font-sans flex flex-col relative h-full">
-      <Header />
-      <main className="flex flex-grow flex-col h-full w-full gap-8 pt-16">
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <>
+      <div className="font-sans flex flex-col relative h-full">
+        <Header />
+        <main className="flex flex-grow flex-col h-full w-full gap-8 pt-16">
+          <Component {...pageProps} />
+        </main>
+      </div>
+      <Analytics mode={'production'} />;
+    </>
   );
 };
 
