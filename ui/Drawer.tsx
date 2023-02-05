7f85508863587cf022ae83a8e23b9303ee2d2159
yarabramasta <yarabram111@gmail.com>
2023-02-05 19:51:00 +0700

    feat(Drawer): organize into separate context

--- ui/Drawer.tsx
+++ ui/Drawer.tsx
@@ -1,5 +1,85 @@
 import { AnimatePresence, motion } from 'framer-motion';
-import { type FC } from 'react';
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+  type FC
+} from 'react';
+
+const context = createContext({ state: false, toggle: () => {} });
+
+const DrawerProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [open, setOpen] = useState(false);
+  const toggle = () => setOpen(!open);
+
+  useEffect(() => {
+    const t = setTimeout(() => {
+      document.body.style.overflowY = open ? 'hidden' : 'scroll';
+    }, 300);
+
+    return () => clearTimeout(t);
+  }, [open]);
+
+  return (
+    <context.Provider value={{ state: open, toggle }}>
+      {children}
+    </context.Provider>
+  );
+};
+
+const dropdown = {
+  hidden: {
+    opacity: 0,
+    height: 0
+  },
+  enter: {
+    opacity: 1,
+    height: 'calc(100% - 4rem)',
+    transition: {
+      layout: {
+        delay: 0.3,
+        type: 'spring',
+        duration: 2.5
+      }
+    }
+  },
+  exit: {
+    opacity: 0,
+    height: 0,
+    transition: {
+      layout: {
+        ease: 'easeOut',
+        duration: 1.5
+      }
+    }
+  }
+};
+
+const Drawer: FC & {
+  Toggle: typeof DrawerToggle;
+  Context: typeof DrawerProvider;
+  state: typeof context;
+} = () => {
+  const { state } = useContext(context);
+
+  return (
+    <AnimatePresence key="drawer">
+      {state && (
+        <motion.ul
+          className="absolute top-16 left-0 w-full p-8 flex flex-col gap-6 backdrop-blur-md bg-white04 z-20"
+          style={{ height: 'calc(100% - 4rem)' }}
+          variants={dropdown}
+          initial="hidden"
+          animate="enter"
+          exit="exit"
+          layout
+        ></motion.ul>
+      )}
+    </AnimatePresence>
+  );
+};
 
 /* ---------- MOTION VARIANTS ---------- */
 const arrowSvg = {
@@ -61,16 +141,17 @@ const ArrowIcon: FC<{ motionKey: string; init: string }> = ({
   );
 };
 
-const DrawerToggle: FC<{
-  // eslint-disable-next-line no-unused-vars
-  toggle: (state: boolean) => void;
-  state: boolean;
-}> & { Icon: typeof ArrowIcon } = ({ toggle, state }) => {
+const DrawerToggle: FC<{ onClick: () => void }> = ({ onClick }) => {
+  const { toggle, state } = useContext(context);
+
   return (
     <button
       key="arrow-button"
       className="p-1 border border-white20 duration-300 ease-in-out rounded-md hover:bg-white04 active:bg-white20 group overflow-hidden"
-      onClick={() => toggle(state)}
+      onClick={() => {
+        onClick();
+        toggle();
+      }}
     >
       <AnimatePresence mode="wait">
         {state ? (
@@ -83,6 +164,8 @@ const DrawerToggle: FC<{
   );
 };
 
-DrawerToggle.Icon = ArrowIcon;
+Drawer.Toggle = DrawerToggle;
+Drawer.Context = DrawerProvider;
+Drawer.state = context;
 
-export default DrawerToggle;
+export default Drawer;
