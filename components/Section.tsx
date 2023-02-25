544d52e4d59eb86f764a39089530e5c94e784f6c
yarabramasta <yarabram111@gmail.com>
2023-02-25 19:19:41 +0700

    feat: first home section

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,6 +1,18 @@
-export default function Section({ children }: React.PropsWithChildren) {
+import clsx from 'clsx';
+
+export default function Section({
+  children,
+  fullHeight = false,
+  border = true
+}: React.PropsWithChildren<{ fullHeight?: boolean; border?: boolean }>) {
   return (
-    <section className="overflow-hidden p-10 lg:p-20 border-b border-b-black/20 dark:border-b-white/20">
+    <section
+      className={clsx(
+        fullHeight && 'safe-h-screen',
+        border ? 'border-b' : 'border-0',
+        'overflow-hidden p-10 lg:p-20 border-b-dark/20 dark:border-b-light/20'
+      )}
+    >
       {children}
     </section>
   );
