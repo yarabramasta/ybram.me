e349ff4e6f3983f867584f54892b94815657e01c
Yara Bramasta <yarabram111@gmail.com>
2023-02-25 19:27:20 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,6 @@ export default function Home() {
       <div className="flex flex-col lg:flex-row border-b border-b-dark/20 dark:border-b-light/20">
         <Section border={false} fullHeight>
           <div className="flex h-full flex-col justify-between">
-            <h3 className="flex flex-row items-center gap-4 text-xs lg:text-sm justify-center lg:justify-start text-dark/80 dark:text-light/80">
-              <span>Yara Bramasta</span>
-              <hr className="border-dark/80 dark:border-light/80 w-6" />
-              <span className="flex-grow">Mobile App Developer</span>
-            </h3>
             <h1 className="text-2xl lg:text-4xl font-semibold max-w-[480px]">
               A Passionate Software Developer,
               <br />
