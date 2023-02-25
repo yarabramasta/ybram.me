40bf1b5294c3c06ce0a42b281f0123b5b090af1e
Yara Bramasta <yarabram111@gmail.com>
2023-02-25 17:54:04 +0700

    fix: typo icon name

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,8 +42,8 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       '/favicon.ico',
-      { url: '/apple-icon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/apple-icon-32x32.png', sizes: '32x32', type: 'image/png' }
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
     ],
     shortcut: getAbsoluteUrl('/favicon.ico'),
     apple: [
