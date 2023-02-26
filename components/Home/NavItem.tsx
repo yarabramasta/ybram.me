ecba6940afddcad36dbab07d5e949f230bdd3c07
Yara Bramasta <yarabram111@gmail.com>
2023-02-26 16:05:13 +0700

    Update NavItem.tsx

--- components/Home/NavItem.tsx
+++ components/Home/NavItem.tsx
@@ -9,7 +9,7 @@ export default function NavItem({
   return (
     <Link
       href={href}
-      className="flex-grow border-r border-r-dark/20 dark:border-r-light/20 h-full p-10 lg:pr-20 flex justify-center md:justify-start items-end duration-300 ease-out hover:bg-dark/10 hover:dark:bg-light/10"
+      className="flex-grow border-r border-r-dark/20 dark:border-r-light/20 h-full p-8 lg:pr-20 flex justify-center md:justify-start items-end duration-300 ease-out hover:bg-dark/10 hover:dark:bg-light/10"
     >
       {children}
     </Link>
