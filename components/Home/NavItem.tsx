0ebfc39e5de51f770c48efea631503aad33cbcb0
Yara Bramasta <yarabram111@gmail.com>
2023-02-26 16:09:37 +0700

    Update NavItem.tsx

--- components/Home/NavItem.tsx
+++ components/Home/NavItem.tsx
@@ -1,15 +1,17 @@
 'use client';
 
 import Link from 'next/link';
+import clsx from 'clsx';
 
 export default function NavItem({
   href,
-  children
-}: React.PropsWithChildren<{ href: string }>) {
+  children,
+  border = true
+}: React.PropsWithChildren<{ href: string, border: boolean }>) {
   return (
     <Link
       href={href}
-      className="flex-grow border-r border-r-dark/20 dark:border-r-light/20 h-full p-8 lg:pr-20 flex justify-center md:justify-start items-end duration-300 ease-out hover:bg-dark/10 hover:dark:bg-light/10"
+      className={clsx(border && 'border-r border-r-dark/20 dark:border-r-light/20', "flex-grow h-full p-8 lg:pr-20 flex justify-center md:justify-start items-end duration-300 ease-out hover:bg-dark/10 hover:dark:bg-light/10")}
     >
       {children}
     </Link>
